42a06d06f99d836e5e226792277e09ca85355dcd
jess79336 <127780039+jess79336@users.noreply.github.com>
2026-08-16 13:33:57 -0600

    feat: add Socket.io gateway with JWT-authenticated real-time sync

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { NotesService } from './notes.service';
 import { NotesController } from './notes.controller';
+import { NotesGateway } from './notes.gateway';
 
 @Module({
-  providers: [NotesService],
+  imports: [JwtModule], 
+  providers: [NotesService, NotesGateway],
   controllers: [NotesController]
 })
 export class NotesModule {}
